a348e48bbd771050abd372d3442b0232b469cd0a
lizhen <lizhn95@163.com>
2017-11-26 11:24:22 +0800

    添加spring配置文件及jar包

---
+++ src/main/java/com/pigeon/testMain/TestMain.java
@@ -0,0 +1,26 @@
+package com.pigeon.testMain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pigeon.module.test.dto.TestDTO;
+import com.pigeon.module.test.service.TestService;
+
+@RestController
+@RequestMapping("/test")
+public class TestMain {
+
+	@Autowired
+	TestService testService;
+	
+	@RequestMapping(value = "/showTest", method = { RequestMethod.POST })
+	public TestDTO showTest(@RequestBody TestDTO request){
+		int id = request.getId();
+		TestDTO t = testService.selectByPrimaryKey(id);
+		return t;
+	}
+
+}
