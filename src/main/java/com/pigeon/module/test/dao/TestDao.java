dc77df4f8f294a224f9eab8552d5e9d495334e8d
lizhen <lizhn95@163.com>
2017-12-01 23:29:40 +0800

    服务调用端添加

--- src/main/java/com/pigeon/module/test/dao/TestDao.java
+++
@@ -1,8 +0,0 @@
-package com.pigeon.module.test.dao;
-
-import com.pigeon.module.test.model.Test;
-
-public interface TestDao {
-	
-	Test selectByPrimaryKey(Integer id);
-}
--- src/main/java/com/pigeon/module/test/service/TestService.java
+++
@@ -1,7 +0,0 @@
-package com.pigeon.module.test.service;
-
-import com.pigeon.module.test.dto.TestDTO;
-
-public interface TestService {
-	TestDTO selectByPrimaryKey(Integer id);
-}
